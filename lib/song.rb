e23fab70954745c02782fcb866e35b2b2fb86585
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-24 22:12:58 -0400

    initialized constants

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,25 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
 
   def self.all
     @@all
   end
 
-  def save
+  def save #saves every instance of self into self.class or Song
     self.class.all << self
   end
 
-end
+  def self.create
+    name
+  end
+end # End of Class
+
+binding.pry
